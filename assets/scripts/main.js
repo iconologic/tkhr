0a03c0019727cee03dd6737da06e30bbb507fa43
JELL K <jayk@tunepusher.com>
2015-08-31 14:39:15 -0400

    Footer links updated

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	//Random bg
     var images = [
     	'bg_1.jpg', 
     	'bg_2.jpg', 
@@ -8,4 +10,24 @@ $(document).ready(function() {
     ];
 
     $('body').css({'background-image': 'url(../assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+    //Category Sprites Hover Anim
+
+		$(function(){
+
+		$('.nav-bar li a').append('<span class="hover"></span>');
+		// span whose opacity will animate when mouse hovers.
+
+		$('.nav-bar li a').hover(
+			function() {
+				$('.hover', this).stop().animate({
+					'opacity': 1 }, 700,'easeOutSine')
+			},
+			function() {
+				$('.hover', this).stop().animate({
+					'opacity': 0 }, 700, 'easeOutQuad')
+
+			})
+		});
+
 });
