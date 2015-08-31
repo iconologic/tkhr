f1aa8ecbb0bedb91d63a982a8fdb172f1ce86d93
Casey Jardin <casey@iconologic.com>
2015-08-31 11:28:37 -0400

    fixed relative path for random backgrounds.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    var images = ['bg_1.jpg', 'bg_2.jpg', 'bg_3.jpg', 'bg_4.jpg', 'bg_5.jpg'];
+    var images = [
+    	'bg_1.jpg', 
+    	'bg_2.jpg', 
+    	'bg_3.jpg', 
+    	'bg_4.jpg', 
+    	'bg_5.jpg'
+    ];
 
-    $('body').css({'background-image': 'url(assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    $('body').css({'background-image': 'url(../assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
