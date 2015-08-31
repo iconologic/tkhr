323e3f754292b182dcde396b3d081a0c7c987461
Casey Jardin <casey@iconologic.com>
2015-08-31 16:02:15 -0400

    More team edits

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,23 +11,17 @@ $(document).ready(function() {
 
     $('body').css({'background-image': 'url(../assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 
-    //Category Sprites Hover Anim
 
-		$(function(){
-
-		$('.nav-bar li a').append('<span class="hover"></span>');
-		// span whose opacity will animate when mouse hovers.
-
-		$('.nav-bar li a').hover(
-			function() {
-				$('.hover', this).stop().animate({
-					'opacity': 1 }, 700,'easeOutSine')
-			},
-			function() {
-				$('.hover', this).stop().animate({
-					'opacity': 0 }, 700, 'easeOutQuad')
-
-			})
-		});
+	// Team Grid Hover
+		$('.team-overlay').css('opacity', 1);  
+		// when hover over the selected image change the opacity to 1  
+		$('.team-overlay').hover(  
+		   function(){  
+		      $(this).fadeTo('fast', 0.4);  
+		   },  
+		   function(){  
+		      $(this).fadeTo('fast', 1);  
+		   });  
+		  
 
 });
