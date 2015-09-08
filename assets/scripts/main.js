996404799528a2c74583ba3d04705fb82a77b57c
Casey Jardin <casey@iconologic.com>
2015-09-08 17:54:01 -0400

    Added services + details templates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+	 //Vertical Tab
+	 $('#practiceAreaTabs').easyResponsiveTabs({
+	     type: 'vertical', //Types: default, vertical, accordion
+	     width: 'auto', //auto or any width like 600px
+	     fit: true, // 100% fit in a container
+	     closed: 'accordion', // Start closed if in accordion view
+	     tabidentify: 'hor_1', // The tab groups identifier
+	     activate: function(event) { // Callback function if tab is switched
+	         var $tab = $(this);
+	         var $info = $('#nested-tabInfo2');
+	         var $name = $('span', $info);
+	         $name.text($tab.text());
+	         $info.show();
+	     }
+	 });
+
 	//Random bg
     var images = [
     	'bg_1.jpg', 
@@ -9,7 +25,7 @@ $(document).ready(function() {
     	'bg_5.jpg'
     ];
 
-    $('body').css({'background-image': 'url(../assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    $('body').css({'background-image': 'url(http://sandbox.iconologic.com/thor/assets/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 
 
 	// Team Grid Hover
@@ -26,7 +42,7 @@ $(document).ready(function() {
 	}
 
 	// Hover Effect for Team Grid
-	$('.team-partner, .team-associate, .team-counsel, .team-patent').hover(hoverIn, hoverOut);
+	$('.team-partner, .team-associate, .team-counsel, .team-patent').hover(hoverIn, hoverOut)
 	  
 
 });
